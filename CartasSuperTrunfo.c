e4d85c80f6728c92c7fd2286e8bb9256274766dd
ProgLab25 <mneryllene@gmail.com>
2025-03-13 15:12:46 +0000

    float - Area printf - Area float - PBI printf - PBI int - Pontos_Turisticos printf - Pontos Turisticos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -29,8 +29,8 @@ int main() {
     printf("Carta 2:\n");
     char Para ='B';
     printf("Para:%c \n",Para);
-    char Codigo[]="B02";
-    printf("Codigo:%s \n",Codigo);
+    char Codigo[] = "B02";
+    printf ("Codigo:%s \n",Codigo);
     
     // Sugestão: Defina Variáveis separadas para cada atributo das cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
